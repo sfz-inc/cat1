d1a693a3ce3a9ebe2ccba61305ce2ed08d886f7c
Ryan Day <jrday@users.noreply.github.com>
2018-08-22 11:04:15 -0700

    Initial commit from Atomist

---
+++ src/main/java/atm/Cat1Application.java
@@ -0,0 +1,12 @@
+package atm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cat1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cat1Application.class, args);
+	}
+}
